6a2f255cfe4f92dbb88bc2b7ebbe4b9da2cad404
jtreeves <jackson.t.reeves@gmail.com>
2021-01-08 14:29:41 -0500

    [my_script] Replace reads with writes

--- my_script.py
+++ my_script.py
@@ -1,7 +1,11 @@
-hello_file = open('hello.txt')
-print(hello_file.read())
+hello_file = open('hello.txt', 'w')
+ga_intro = 'Welcome to General Assembly'
+hello_file.write(ga_intro)
+# print(hello_file.read())
 hello_file.close()
 
-car_file = open('car.txt')
-print(car_file.read())
+car_file = open('car.txt', 'w')
+new_car_list = 'electric\ngas\nhybrid'
+car_file.write(new_car_list)
+# print(car_file.read())
 car_file.close()
